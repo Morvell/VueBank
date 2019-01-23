34aea9e25c80453903562e588915183c40796254
morvell <zaxarov2702@yandex.ru>
2019-01-23 23:07:14 +0500

    2

--- src/main/java/morvell/vuebank/VueBankApplication.java
+++ src/main/java/morvell/vuebank/VueBankApplication.java
@@ -1,25 +1,23 @@
 package morvell.vuebank;
 
-import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
 import javax.jms.Queue;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import sun.rmi.runtime.Log;
 
 @Slf4j
 @SpringBootApplication
 public class VueBankApplication {
 
-	@Bean
-	public Queue queue() {
-		return new ActiveMQQueue("sample.queue");
-	}
+  @Bean
+  public Queue queue() {
+    return new ActiveMQQueue("sample.queue");
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(VueBankApplication.class, args);
-		log.info("start!!!!!!!!!!!!");
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(VueBankApplication.class, args);
+    log.info("start!!!!!!!!!!!!");
+  }
 }
